92cbd72f505ad2dc58db2483ddc9318efa47f274
SirHexxus <jamesmichaelstacy@gmail.com>
2022-01-28 15:18:40 -0800

    added shebang and def main if __name__ == __main__ for better use as a script

--- weather.py
+++ weather.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import requests
 import sys
@@ -26,44 +28,51 @@ def kelvin_to_fahrenheit(temp):
 def mps_to_mph(speed):
     return round(speed * 2.237, 2)
 
+# Main function
 
-# Logic ladder for parsing the arguments passed to the script, and getting the city name
-if len(sys.argv) == 1:
-    city = input("Enter city name: ").lower()
-elif len(sys.argv) == 2:
-    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-        print(HELP_MSG)
-        sys.exit(0)
-    elif sys.argv[1] == 'here':
-        get_location = requests.get('http://ipinfo.io')
-        city = get_location.json().get('city').lower()
+
+def main():
+    # Logic ladder for parsing the arguments passed to the script, and getting the city name
+    if len(sys.argv) == 1:
+        city = input("Enter city name: ").lower()
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print(HELP_MSG)
+            sys.exit(0)
+        elif sys.argv[1] == 'here':
+            get_location = requests.get('http://ipinfo.io')
+            city = get_location.json().get('city').lower()
+        else:
+            city = sys.argv[1].lower()
     else:
-        city = sys.argv[1].lower()
-else:
-    print(HELP_MSG)
-    sys.exit(1)
+        print(HELP_MSG)
+        sys.exit(1)
 
-# Define the URL to get the weather data from
-request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+    # Define the URL to get the weather data from
+    request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
 
-# Get the weather data from the API
-response = requests.get(request_url)
+    # Get the weather data from the API
+    response = requests.get(request_url)
 
-# Check if the city was NOT found, and exit the script if it wasn't
-if response.status_code != 200:
-    print("Error:", response.status_code)
-    sys.exit(1)
+    # Check if the city was NOT found, and exit the script if it wasn't
+    if response.status_code != 200:
+        print("Error:", response.status_code)
+        sys.exit(1)
 
-# Parse the JSON response
-data = response.json()
-weather = data["weather"]
-description = weather[0]["description"]
-windspeed = data["wind"]["speed"]
-temperature = data["main"]["temp"]
+    # Parse the JSON response
+    data = response.json()
+    weather = data["weather"]
+    description = weather[0]["description"]
+    windspeed = data["wind"]["speed"]
+    temperature = data["main"]["temp"]
 
-# Print the weather data
-print(city.title())
-print(f"Weather\n\t{description.title()}")
-print(f"Wind Speed\n\t{mps_to_mph(windspeed)} mph\n\t{windspeed} m/s")
-print(
-    f"Temperature:\n\t{kelvin_to_fahrenheit(temperature)} F\n\t{kelvin_to_celsius(temperature)} C")
+    # Print the weather data
+    print(city.title())
+    print(f"Weather\n\t{description.title()}")
+    print(f"Wind Speed\n\t{mps_to_mph(windspeed)} mph\n\t{windspeed} m/s")
+    print(
+        f"Temperature:\n\t{kelvin_to_fahrenheit(temperature)} F\n\t{kelvin_to_celsius(temperature)} C")
+
+
+if __name__ == "__main__":
+    main()
